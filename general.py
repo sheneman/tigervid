f2125f98f5cb74473ce844cfaffdfce4f7b27dd2
sheneman <sheneman@uidaho.edu>
2023-10-26 09:18:14 -0700

    fixed reference to missing LOGGING function

--- general.py
+++ general.py
@@ -107,7 +107,7 @@ def non_max_suppression(prediction,
 
         output[xi] = x[i]
         if (time.time() - t) > time_limit:
-            LOGGER.warning(f'WARNING: NMS time limit {time_limit:.3f}s exceeded')
+            #LOGGER.warning(f'WARNING: NMS time limit {time_limit:.3f}s exceeded')
             break  # time limit exceeded
 
     return output
